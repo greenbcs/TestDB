041c88f2d9cdd2e5fe5b2d8b6908f792966ef75b
Green <greenbcs@gmail.com>
2018-03-12 07:03:21 +0800

    一次提交多条入库
    修改PMT_storage和PMT_storage_log的num计算

--- backend/PMTs-Storage.php
+++ backend/PMTs-Storage.php
@@ -5,63 +5,74 @@ $conn=mysqli_connect($server,$ur,$psw,$datab);
 session_start();
 $junoL23=$_SESSION["JUNOLEVEL"];
 if($junoL23=="3"|$junoL23=="2") {
-$sql1 = mysqli_query($conn,"select * from pmts_storage") or die(mysqli_connect_error());
-$row1 = mysqli_num_rows($sql1);
-if($row1==0){
-    $num=1;
+	$sql = mysqli_query($conn,"select * from pmts_storage") or die(mysqli_connect_error());
+	$row1 = mysqli_num_rows($sql);
+	if($row1==0){
+		$num=1;
+	}
+	else{
+		$sql1 = mysqli_query($conn,"select * from pmts_storage where NO=(select max(NO) from pmts_storage) ");
+		while($row=mysqli_fetch_assoc($sql1)){
+			$num1=$row['NO']+1;
+		}
+		$sql2 = mysqli_query($conn,"select * from pmts_storage_logs where NO=(select max(NO) from pmts_storage_logs) ");
+		while($row=mysqli_fetch_assoc($sql2)){
+		    $num2=$row['NO']+1;
+		}
+	}
+	$SR =$_POST['SR'];
+	$SW =$_POST['SW'];
+	$SP =$_POST['SP'];
+	$Status =$_POST['Status'];
+	$Stage =$_POST['Stage'];
+	$SNote =$_POST['SNote'];
+	date_default_timezone_set('Asia/Shanghai');
+	$SD = date("Y-m-d H:i:s",time());
+	$SNs = explode(",", $_POST['SNs']);
+	$number=count($SNs);
+	for ($i=0;$i<$number;$i++)
+	{
+		$SN = $SNs[$i];
+		$sql3 = "select * from receive_check where SN = '$SN'";
+		$result = mysqli_query($conn,$sql3);
+		$row2 =  mysqli_num_rows($result);
+		if ($row2 == 0)
+		{
+			echo "<script>alert('We did not receive this PMT!');location.href='".$_SERVER["HTTP_REFERER"]."#query-storage' </script>";;
+			//echo $SN,"is not in store.\n";
+		}
+		else
+		{
+			$sql4="select BN from receive_check where SN = '$SN'";
+			$result = mysqli_query($conn,$sql4);
+			while($row=mysqli_fetch_assoc($result)){
+				$BN=$row['BN'];
+			};
+			$sql5="insert into pmts_storage_logs (NO,SN,SR,SW,BN,SP,Location,Stage,SNote,SD) values ('$num2','$SN','$SR','$SW','$BN','$SP','$Status','$Stage','$SNote','$SD')";
+			//echo $sql5,"<br>";
+			$sql6="insert into pmts_storage (NO,SN,SR,SW,BN,SP,Location,Stage,SNote,SD) values ('$num1','$SN','$SR','$SW','$BN','$SP','$Status','$Stage','$SNote','$SD') ON DUPLICATE KEY UPDATE SN='$SN',SR='$SR',SW='$SW',BN='$BN',SP='$SP',Location='$Status',Stage='$Stage',SNote='$SNote',SD='$SD'";
+			//echo $sql6;
+			if (!mysqli_query($conn,$sql5))
+			{
+				die('Error: ' . mysqli_error($conn));
+			}
+			else
+			{
+				print "<script>location.href='".$_SERVER["HTTP_REFERER"]."#PMTs-storage' </script>";
+				//echo "Success to insert ",$SN,"!\n";
+			};
+			if (!mysqli_query($conn,$sql6))
+			{
+				die('Error: ' . mysqli_error($conn));
+			};
+		};
+		$num1 = $num1 + 1;
+		$num2 = $num2 + 1;
+	};
 }
 else{
-    $sql2 = mysqli_query($conn,"select * from pmts_storage where NO=(select max(NO) from pmts_storage) ");
-    while($row=mysqli_fetch_assoc($sql2)){
-        $num=$row['NO']+1;
-        //echo $num;
-    }
-
-}
-//echo $row1;
-//echo $url;
-$SN = @$_GET['SN'];
-$SR =@$_GET['SR'];
-$SW =@$_GET['SW'];
-$SD =@$_GET['SD'];
-$SP =@$_GET['SP'];
-$Status =@$_GET['Status'];
-$Stage =@$_GET['Stage'];
-$SNote =@$_GET['SNote'];
-//echo $WHN;
-$sql0="select * from receive_check where SN ='$SN'";
-$result=mysqli_query($conn,$sql0);
-
-//$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-if (!mysqli_num_rows($result))
-{
-    //echo "<script>alert('We did not receive this PMT!');history.back()</script>";
-    echo "<script>alert('We did not receive this PMT!');location.href='".$_SERVER["HTTP_REFERER"]."' </script>";;
-}
-else
-{
-    $sql6="insert into pmts_storage_logs (NO,SN,SR,SW,SD,SP,Location,Stage,SNote) values ('$num','$SN','$SR','$SW','$SD','$SP','$Status','$Stage','$SNote')";
-    $sql="insert into pmts_storage (NO,SN,SR,SW,SD,SP,Location,Stage,SNote) values ('$num','$SN','$SR','$SW','$SD','$SP','$Status','$Stage','$SNote') ON DUPLICATE KEY UPDATE SN='$SN',SW='$SW',SR='$SR',SD='$SD',SP='$SP',Location='$Status',Stage='$Stage',SNote='$SNote'";
-   if (!mysqli_query($conn,$sql))
-    {
-       die('Error: ' . mysqli_error($conn));
-     }
-    else{print "<script>alert('Add Success!');location.href='".$_SERVER["HTTP_REFERER"]."' </script>";}
-    if (!mysqli_query($conn,$sql6))
-    {
-        die('Error: ' . mysqli_error($conn));
-    }
-}
-}
-else{
-    print "<script>alert('Your authorization or permissions are too low to operate it.');location.href='".$_SERVER["HTTP_REFERER"]."'</script>";
-}
-//$sql="insert into pmts_storage (NO,SN,SR,SD,SP,SNote) values ('$row1','$SN','$SR','$SD','$SP','$SNote')";
-//if (!mysqli_query($conn,$sql))
-//{
- //   $mes=die('Error: ' . mysqli_error($conn));
-//}
-//else{print "<script>alert('Add Success!');location.href='".$_SERVER["HTTP_REFERER"]."' </script>";}
+    print "<script>alert('Your authorization or permissions are too low to operate it.');location.href='".$_SERVER["HTTP_REFERER"]."#404'</script>";
+};
+mysqli_close($conn);
 ?>
 
